c402bf404554edf760a723c3569817c1f9e7d180
cameronheine <70171437+cameronheine@users.noreply.github.com>
2020-12-07 23:06:54 -0800

    Update CoinFlip(2).js

--- CoinFlip(2).js
+++ CoinFlip(2).js
@@ -1,19 +1,27 @@
 document.getElementById('click').onclick = click;
  var heads = 0;
  var tails = 0;
+ var myObj;
+ myObj = {
+	 "name": "Coin",
+	 "Side": {"side1": "Heads", "side2": "Tails"}
+ }
  function click() {
     x = Math.random();
     if(x <= 0.5) {
 	   document.getElementById("coin").src = "half_dollar_head.png";
 	   console.log("It's Heads");
+	   document.getElementById("demo").innerHTML = myObj.Side.side1;
 	 }
 	else{
 	   document.getElementById("coin").src = "half_dollar_tail.png";
 	   console.log("It's tails");
+	   document.getElementById("demo").innerHTML = myObj.Side.side2;
 	 } 
  };
  function click2() {
 	 document.getElementById("coin").src = "unnamed.png";
 	 console.clear();
+	 document.getElementById("demo").innerHTML = " ";
  };
 	
